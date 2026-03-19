fe891641dfde158f805d8866e83bb0b04d2639b6
Sinad04 <daniel.schwenkkrauss@gmail.com>
2026-03-19 21:10:07 +0100

    feat: added better response and error handling to the robots cache
    modified:   WebCrawler/RobotsCache.cs

--- WebCrawler/RobotsCache.cs
+++ WebCrawler/RobotsCache.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Net;
 using RobotsTxtParser;
 
 namespace WebCrawler;
@@ -7,19 +8,44 @@ public class RobotsCache
 {
     private readonly ConcurrentDictionary<string, Robots> _robotsCache = new();
     private readonly HttpClient _client;
-
+    
     public RobotsCache(HttpClient client) { _client = client; }
     
-    public async Task<Robots> TryGetRobotsAsync(string baseUrl)
+    public async Task<Robots> TryGetRobotsAsync(string baseUrl, CancellationToken ctoken)
     {
         Log.Info($"Querying robots cache for {baseUrl}");
-        if (_robotsCache.TryGetValue(baseUrl, out var robots)) return robots;
+        if (_robotsCache.TryGetValue(baseUrl, out var cachedRobots)) return cachedRobots;
         Log.Info($"{baseUrl} not found in robots cache. Attempting to retrieve robots.txt from web server.");
-        var robotsString = await _client.GetStringAsync($"{baseUrl}/robots.txt"); // TODO handle unsuccessful request
+
+        Robots? robots = null;
         
-        robots = new Robots(robotsString);
-        _robotsCache.TryAdd(baseUrl, robots); // TODO maybe a way to avoid spamming robots.txt requests should caching fail. though idk if it ever would that badly
-        return robots; 
+        var robotsResponseMessage = await _client.GetAsync($"{baseUrl}/robots.txt", ctoken);
+
+        if (robotsResponseMessage.IsSuccessStatusCode)
+        {
+            var robotsString = await robotsResponseMessage.Content.ReadAsStringAsync(ctoken);
+            robots = new Robots(robotsString);
+        } 
+        else
+        {
+            switch (robotsResponseMessage.StatusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    Log.ImportantInfo($"No robots.txt found at {baseUrl}. Assuming every path is allowed.");
+                    robots = new Robots(""); // If no robots.txt is provided by the domain, then assume Crawler is allowed everywhere.
+                    break;
+                case HttpStatusCode.TooManyRequests:
+                    return new Robots("User-agent: *\r\nDisallow: \\"); // If client is temporarily rate-limited, do not cache.
+            }
+        }
+        if (robots is null)
+        {
+            Log.Warn($"Could not acquire robots.txt for {baseUrl}. Assuming Crawler not allowed on domain.");
+            robots = new Robots("User-agent: *\r\nDisallow: \\"); // If robots.txt couldn't be acquired, to be safe, assume for this domain that Crawler isn't allowed.
+        }
+        
+        _robotsCache.TryAdd(baseUrl, robots);
+        return robots;
     }
 }
 
