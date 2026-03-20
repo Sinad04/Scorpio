853f0e321dc996620d41d209857679cac7acf192
Sinad04 <daniel.schwenkkrauss@gmail.com>
2026-03-20 13:50:46 +0100

    feat: robots cache now checks for redirects
    modified:   WebCrawler/RobotsCache.cs

--- WebCrawler/RobotsCache.cs
+++ WebCrawler/RobotsCache.cs
@@ -18,26 +18,44 @@ public class RobotsCache
         Log.Info($"{baseUrl} not found in robots cache. Attempting to retrieve robots.txt from web server.");
 
         Robots? robots = null;
+        var queriedBaseUrl = baseUrl;
+        var redirects = 0;
         
-        var robotsResponseMessage = await _client.GetAsync($"{baseUrl}/robots.txt", ctoken);
+        while (robots is null && redirects <= 3)
+        {
+            using var robotsResponseMessage = await _client.GetAsync($"{queriedBaseUrl}/robots.txt", ctoken);
 
-        if (robotsResponseMessage.IsSuccessStatusCode)
-        {
-            var robotsString = await robotsResponseMessage.Content.ReadAsStringAsync(ctoken);
-            robots = new Robots(robotsString);
-        } 
-        else
-        {
-            switch (robotsResponseMessage.StatusCode)
+            if (robotsResponseMessage.IsSuccessStatusCode)
             {
-                case HttpStatusCode.NotFound:
-                    Log.ImportantInfo($"No robots.txt found at {baseUrl}. Assuming every path is allowed.");
-                    robots = new Robots(""); // If no robots.txt is provided by the domain, then assume Crawler is allowed everywhere.
-                    break;
-                case HttpStatusCode.TooManyRequests:
-                    return new Robots("User-agent: *\r\nDisallow: \\"); // If client is temporarily rate-limited, do not cache.
+                Log.Info($"{queriedBaseUrl} has robots.txt.");
+                var robotsString = await robotsResponseMessage.Content.ReadAsStringAsync(ctoken);
+                robots = new Robots(robotsString);
+            } 
+            else
+            {
+                switch (robotsResponseMessage.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        Log.Info($"No robots.txt found at {baseUrl}. Checking for redirect.");
+
+                        var redirectedBaseUrlString = await CheckForRedirectedBaseUrlAsync(baseUrl, ctoken);
+                        
+                        if (string.IsNullOrWhiteSpace(redirectedBaseUrlString) && redirectedBaseUrlString != baseUrl) 
+                        { 
+                            queriedBaseUrl = redirectedBaseUrlString; 
+                            redirects++; 
+                            continue;
+                        }
+                        Log.Warn($"No robots.txt found for {baseUrl}. Assuming Crawler allowed everywhere.");
+                        robots ??= new Robots(""); // If no robots.txt is provided by the domain, then assume Crawler is allowed everywhere.
+                        break;
+                    
+                    case HttpStatusCode.TooManyRequests:
+                        return new Robots(""); // If client is temporarily rate-limited, do not cache.
+                }
             }
         }
+        
         if (robots is null)
         {
             Log.Warn($"Could not acquire robots.txt for {baseUrl}. Assuming Crawler not allowed on domain.");
@@ -46,6 +64,16 @@ public class RobotsCache
         
         _robotsCache.TryAdd(baseUrl, robots);
         return robots;
+    }
+
+    private async Task<string> CheckForRedirectedBaseUrlAsync(string baseUrl, CancellationToken ctoken)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Head, baseUrl);
+        using var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, ctoken);
+        
+        var redirectedBaseUrl = response?.RequestMessage?.RequestUri;
+        return Util.NormalizeUrl(redirectedBaseUrl?.ToString() ?? baseUrl);
+        
     }
 }
 
